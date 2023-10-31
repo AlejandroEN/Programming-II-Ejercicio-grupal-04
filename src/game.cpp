825549c10a0b9cbd0f501d5db031404b77c5d8aa
Alejandro E. <alejandroescobarnunez@gmail.com>
2023-10-30 20:35:51 -0500

    turnos fixed

--- src/game.cpp
+++ src/game.cpp
@@ -2,15 +2,22 @@
 
 void runGame(Player *jugador1, Player *jugador2)
 {
+    int turn = 1;
+    Player *attackPlayer;
+    Player *defendPlayer;
+
     while (jugador1->getCoins() > 0 && jugador2->getCoins() > 0)
     {
+        attackPlayer = turn % 2 == 0 ? jugador1 : jugador2;
+        defendPlayer = turn % 2 == 0 ? jugador2 : jugador1;
+
         cout << "-----" << endl;
-        auto dicesAttackAmount = input<int>(format("{}, ¿con cuántos dados ataca?", jugador1->getNombre()));
+        auto dicesAttackAmount = input<int>(format("{}, ¿con cuántos dados ataca?", attackPlayer->getNombre()));
         vector<int> attackDiceNumbers = Player::getDiceNumbers(dicesAttackAmount);
 
-        auto dicesDefendAmount = input<int>(format("{}, ¿con cuántos dados defiende?", jugador2->getNombre()));
+        auto dicesDefendAmount = input<int>(format("{}, ¿con cuántos dados defiende?", defendPlayer->getNombre()));
         vector<int> defendDiceNumbers = Player::getDiceNumbers(dicesDefendAmount);
-
-
+        
+        turn ++;
     }
 }
